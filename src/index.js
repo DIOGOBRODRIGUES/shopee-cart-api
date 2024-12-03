b6fe22a17f1843316ea4ac376f4a43562d01a862
diogo.borba <diogo.borbar@gmail.com>
2024-12-02 22:52:40 -0300

    implementando fucao de deletar item

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
+import * as cartService from "./services/cart.js";
 import createItem from "./services/item.js";
 
-const cart = [];
+const myCart = [];
+const myWhishList = []; 
 
 console.log("Welcome to the your shopee cart")
 
 const item1 = await createItem("geladeira", 1999.90, 1)
 const item2 = await createItem("torradeira", 53.70, 2)
 
-console.log(item2.subTotal());
+
+await cartService.addItem(myCart, item1);
+await cartService.addItem(myWhishList, item2);
+
+console.log("shopee cart Total is : ");
+await cartService.calculateTotal(myCart);
--- src/services/cart.js
+++ src/services/cart.js
@@ -1,13 +1,14 @@
 //acoes que o carrinho pode fazer 
 
-// -> adicionar item do carrinho
-async function addItem(userCart) {
-    
+// ✅-> adicionar item do carrinho
+async function addItem(userCart, item) {
+    userCart.push(item)
 }
 
 // -> deletar item do carrinho
 
 async function deleteItem(userCart, name) {
+    const index = userCart.findIndex((item)=> item.name === name);
     
 }
 
@@ -17,6 +18,13 @@ async function removeItem(userCart, index) {
 }
 // -> calcular o total do carrinho 
 async function calculateTotal(userCart) {
-    
+    console.log(userCart.reduce((total, item) => total + item.subTotal(), 0));
+}
+
+export{
+    addItem,
+    deleteItem,
+    removeItem,
+    calculateTotal,
 }
 
